76da9f4967c3c1c829f41516f303178bccde591b
NaolGB <naolgbasaye@gmail.com>
2022-10-31 13:19:49 -0400

    CICD test, update on push

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(reques):
-    return HttpResponse('Hi, just putting last touches. Come back a little later 🙂')
+    return HttpResponse('Hi, just putting last touches. Come back a little later 🙂.')
