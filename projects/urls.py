b537e258a396bd04691045a283af64d4cc7f280b
NaolGB <naolgbasaye@gmail.com>
2022-11-02 23:19:57 -0400

    static file serving implemented

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='projects_home'),
     path('blinder_resume/', views.blinderResume, name='projects_blinder_resume'),
+    path('web_map/', views.webMap, name='projects_web_map'),
+    path('crypto_ml/', views.cryptoML, name='projects_crypto_ml'),
+    path('bart/', views.bart, name='projects_bart'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -5,3 +5,12 @@ def home(request):
 
 def blinderResume(request):
     return render(request, 'projects/blinder_resume.html')
+
+def webMap(request):
+    return render(request, 'projects/web_map.html')
+
+def cryptoML(request):
+    return render(request, 'projects/crypto_ml.html')
+
+def bart(request):
+    return render(request, 'projects/bart.html')
