d1eaf9d1a5929440e47f6f34723ddbe3ed6e0d48
NaolGB <naolgbasaye@gmail.com>
2022-11-26 22:20:52 -0500

    whitenoise for static file serving in prod

--- bynaol/settings.py
+++ bynaol/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['bynaol.com', 'www.bynaol.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
